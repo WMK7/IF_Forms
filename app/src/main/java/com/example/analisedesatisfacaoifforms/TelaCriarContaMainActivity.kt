f2b3da6012e4da0a35fdd3eb100ab0ea614b35b3
Michael <micaelmm36@gmail.com>
2024-11-15 08:37:21 -0400

    telas do IF_Forms Prontas

---
+++ app/src/main/java/com/example/analisedesatisfacaoifforms/TelaCriarContaMainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.analisedesatisfacaoifforms
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TelaCriarContaMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.tela_criar_conta_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
