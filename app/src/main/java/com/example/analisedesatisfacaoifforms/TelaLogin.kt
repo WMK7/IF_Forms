f78fced6ce4d0d2a5e05ede06128dcf95996c2da
Michael <micaelmm36@gmail.com>
2024-11-18 16:59:17 -0400

    tela questão funcionando por enquanto sem programação

--- app/src/main/java/com/example/analisedesatisfacaoifforms/TelaLogin.kt
+++ app/src/main/java/com/example/analisedesatisfacaoifforms/TelaLogin.kt
@@ -3,6 +3,7 @@ package com.example.analisedesatisfacaoifforms
 import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.analisedesatisfacaoifforms.databinding.ActivityTelaLoginBinding
 
@@ -11,15 +12,16 @@ class TelaLogin : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        val email =findViewById<EditText>(R.id.editTextEmailLogin)
-        val senha =findViewById<EditText>(R.id.editTextSenhaLogin)
+        super.onCreate(savedInstanceState)
+        binding = ActivityTelaLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val email = findViewById<EditText>(R.id.editTextEmailLogin)
+        val senha = findViewById<EditText>(R.id.editTextSenhaLogin)
         val especEmail = "mk@gmail.com"
         val especSenha = "123456"
 
 
-        super.onCreate(savedInstanceState)
-        binding = ActivityTelaLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         binding.voltarLogin.setOnClickListener {
             val intent = Intent(this, TelaCriarContaMainActivity::class.java)
             startActivity(intent)
@@ -33,7 +35,10 @@ class TelaLogin : AppCompatActivity() {
             if(email.text.toString() == especEmail && senha.text.toString() == especSenha){
                 val intent = Intent(this, Questionario::class.java)
                 startActivity(intent)
+            }else{
+                Toast.makeText(this, "Email ou senha incorretos", Toast.LENGTH_SHORT).show()
             }
+
         }
 
 
